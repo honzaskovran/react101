cb88b5d784468116fb95389e87b059d807ae7742
Honza Škovran <honza@skovran.net>
2017-01-31 17:38:25 +0100

    set dictators directly to DictatorsList

--- src/js/main.js
+++ src/js/main.js
@@ -2,51 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DictatorsList from './DictatorsList.jsx';
 
-class App extends React.Component {
-    constructor() {
-        super();
+const dictators = [
+    {
+        name: 'Adolf Hitler',
+        country: 'German Reich'
+    },
+    {
+        name: 'Joseph Stalin',
+        country: 'Soviet Union'
+    },
+    {
+        name: 'Mao Zedong',
+        country: 'China'
+    },
+    {
+        name: 'Benito Mussolini',
+        country: 'Italy'
+    },
+    {
+        name: 'Pol Pot',
+        country: 'Cambodia'
+    },
+    {
+        name: 'Kim Jong-il',
+        country: 'North Korea'
+    },
+    {
+        name: 'Saddam Hussein',
+        country: 'Iraq'
+    },
+    {
+        name: 'Robert Mugab',
+        country: 'Zimbabwe'
+    },
+];
 
-        this.state = {
-            dictators: [
-                {
-                    name: 'Adolf Hitler',
-                    country: 'German Reich'
-                },
-                {
-                    name: 'Joseph Stalin',
-                    country: 'Soviet Union'
-                },
-                {
-                    name: 'Mao Zedong',
-                    country: 'China'
-                },
-                {
-                    name: 'Benito Mussolini',
-                    country: 'Italy'
-                },
-                {
-                    name: 'Pol Pot',
-                    country: 'Cambodia'
-                },
-                {
-                    name: 'Kim Jong-il',
-                    country: 'North Korea'
-                },
-                {
-                    name: 'Saddam Hussein',
-                    country: 'Iraq'
-                },
-                {
-                    name: 'Robert Mugab',
-                    country: 'Zimbabwe'
-                },
-            ]
-        };
-    }
-    render() {
-        return <DictatorsList dictators={this.state.dictators}/>
-    }
-}
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<DictatorsList dictators={dictators}/>, document.getElementById('app'));
 
