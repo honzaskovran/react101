388c9e6bc5bf0576b10ba3313c12a0b0dbc3f982
Honza Škovran <honza@skovran.net>
2017-01-31 17:26:34 +0100

    use state in main.js and use it instead of inlined dictators

--- src/js/DictatorsList.jsx
+++ src/js/DictatorsList.jsx
@@ -7,9 +7,7 @@ export default class DictatorsList extends React.Component {
         return <div>
             <h1>List of dictators</h1>
             <ul>
-                <Dictator name="Adolf Hitler" country="German Reich"/>
-                <Dictator name="Joseph Stalin" country="Soviet Union"/>
-                <Dictator name="Mao Zedong" country="China"/>
+                {this.props.dictators.map((value, index) => <Dictator key={index} name={value.name} country={value.country}/>)}
             </ul>
         </div>
     }
--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom';
 import DictatorsList from './DictatorsList.jsx';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            dictators: [
+                {
+                    name: 'Adolf Hitler',
+                    country: 'German Reich'
+                },
+                {
+                    name: 'Joseph Stalin',
+                    country: 'Soviet Union'
+                },
+                {
+                    name: 'Mao Zedong',
+                    country: 'China'
+                },
+                {
+                    name: 'Benito Mussolini',
+                    country: 'Italy'
+                },
+                {
+                    name: 'Pol Pot',
+                    country: 'Cambodia'
+                },
+                {
+                    name: 'Kim Jong-il',
+                    country: 'North Korea'
+                },
+                {
+                    name: 'Saddam Hussein',
+                    country: 'Iraq'
+                },
+                {
+                    name: 'Robert Mugab',
+                    country: 'Zimbabwe'
+                },
+            ]
+        };
+    }
     render() {
-        return <DictatorsList/>
+        return <DictatorsList dictators={this.state.dictators}/>
     }
 }
 
